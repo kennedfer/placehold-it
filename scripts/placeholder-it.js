d83c2f91f490fbe7e391e0f1faede75e1a69970a
kennedfer <kenned14santos@gmail.com>
2024-11-17 13:55:11 -0300

    feat: Add important inputs

--- scripts/placeholder-it.js
+++ scripts/placeholder-it.js
@@ -1,32 +1,45 @@
+function drawCanvas(width, height) {
+  const message = inputText.value || (width + " X " + height)
+  const fontSize = inputFontSize.value || width / 25;
 
+  ctx.fillStyle = inputBackground.value;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = inputForeground.value;
+  ctx.font = `${fontSize}px Arial`;
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(message, canvas.width / 2, canvas.height / 2);
+}
+
+function generatePlaceholder() {
+  const width = inputWidth.value;
+  const height = inputHeight.value;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  const containerAspect = container.offsetWidth / container.offsetHeight;
+  const canvasAspect = width / height;
+
+  if (canvasAspect > containerAspect) {
+    canvas.style.width = "100%"
+    canvas.style.height = "auto"
+  } else {
+    canvas.style.width = "auto"
+    canvas.style.height = "100%"
+  }
+
+  drawCanvas(width, height);
+}
 
 const container = document.getElementById("container-canvas");
 const canvas = document.getElementById("placeholder-canvas");
 const ctx = canvas.getContext("2d");
 
-const width = 1280;
-const height = 720;
-
-canvas.width = width;
-canvas.height = height;
-
-const containerAspect = container.offsetWidth / container.offsetHeight;
-const canvasAspect = width / height;
-
-if (canvasAspect > containerAspect) {
-  canvas.style.width = "100%"
-  canvas.style.height = "auto"
-} else {
-  canvas.style.width = "auto"
-  canvas.style.height = "100%"
-}
-
-ctx.fillStyle = "#4caf50";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.fillStyle = "#fff";
-ctx.font = "40px Arial";
-ctx.textAlign = "center";
-ctx.textBaseline = "middle";
-ctx.fillText("1000x1000", canvas.width / 2, canvas.height / 2);
-
-console.log(containerAspect, canvasAspect);
+const inputWidth = document.getElementById("input-width");
+const inputHeight = document.getElementById("input-height");
+const inputText = document.getElementById("input-text");
+const inputBackground = document.getElementById("input-background-color");
+const inputForeground = document.getElementById("input-foreground-color");
+const inputFontSize = document.getElementById("input-font-size");
